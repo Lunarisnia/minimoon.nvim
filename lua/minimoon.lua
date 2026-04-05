bf4e5c599bd335e0043c7b8be7c787ea03255524
Lounarisnia <r.arswendo.r@gmail.com>
2026-04-05 18:20:03 +0700

    feat: set default binding and defaults states

--- lua/minimoon.lua
+++ lua/minimoon.lua
@@ -3,11 +3,8 @@ This is the config main entry point
 ]]
 --
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+require("config.global-defaults")
+require("keybindings.global-bindings")
 
 -- Initiate lazy package manager
 require("config.lazy")
