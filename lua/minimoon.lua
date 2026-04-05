308775d12da0d00117e57e8aebe2137858cc5d11
Lounarisnia <r.arswendo.r@gmail.com>
2026-04-05 14:46:35 +0700

    feat: lazy setup

---
+++ lua/minimoon.lua
@@ -0,0 +1,13 @@
+--[[ 
+This is the config main entry point
+]]
+--
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Initiate lazy package manager
+require("config.lazy")
